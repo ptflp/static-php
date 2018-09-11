b9b0543f9614475e4923845384f9649c4ce2b331
ptflp <filippov.pa@s-vfu.ru>
2018-09-11 12:52:00 +0900

    Early/Late static binding

---
+++ lsb.php
@@ -0,0 +1,32 @@
+<?php
+class Model {
+  public static $table = 'table';
+  public static function getTable() {
+    return self::$table;
+  }
+}
+
+echo 'Model::getTable()='.Model::getTable(); // 'table'
+echo '<br>';
+
+
+class User extends Model {
+  public static $table = 'users';
+}
+
+echo 'User::getTable()='.User::getTable(); // 'table' gets Model static method. Early static binding self
+
+
+class ModelTwo {
+  public static $table = 'table';
+  public static function getTable() {
+    return static::$table;
+  }
+}
+
+class UserTwo extends ModelTwo {
+  public static $table = 'users';
+}
+
+echo '<br>';
+echo 'UserTwo::getTable()='.UserTwo::getTable(); // 'table' gets UserTwo static method. Late static binding
